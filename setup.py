0cac75c82fe74f461ba2319d0b5acb392c407c55
EduardoZeti <eduardo@zeti.software>
2021-12-17 14:55:09 -0600

    setup updated

--- setup.py
+++ setup.py
@@ -24,18 +24,18 @@ readme = open("./README.md", "r")
 setup(
     name='zarena',
     packages=['zarena'],
-    version="1.0.0",
-    description= '',
+    version="0.1.0",
+    description= 'Rust Game Collection with Reninforcement Learning gym environments. This library aims to serve the same purpose as OpenSpiel, except in Rust to make it easier to use & maintain. The current games are gato, blackjack, chess & poker texas hold'em. All of these additionally support Web Assembly. You can play gato & chess against our Artificial Intelligence at Zeti Games',
     long_description=readme.read(),
     long_description_content_type='text/markdown',
     author='zetiworld',
-    author_email='',
-    url='',
-    download_url='',
-    keywords=[],
+    author_email='zetiapp@gmail.com',
+    url='https://github.com/ZetiAi/zarena/tree/feature/clean-code',
+    download_url='https://github.com/ZetiAi/zarena/releases',
+    keywords=["rust", "performance", "games", "wasm", "gym-environments", "gyms", "maturin"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 1 - Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Rust",
@@ -46,6 +46,6 @@ setup(
         RustExtension("zarena.zarena", "Cargo.toml", binding=Binding.PyO3, debug=False)
     ],
     zip_safe=False,
-    install_requires=["gym>=0,<1", "numpy>=1,<2", "six>=1,<2"],
+    install_requires=["numpy=1.21.1","gym=0.21.0","six=1.16.0","gy=1.0.2","Pillow=8.4.0","tqdm=4.62.3"],
     extras_require={"dev": ["pytest", "black"]}
 )
