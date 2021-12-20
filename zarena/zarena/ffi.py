b87cf333e8d7ecb4db8d49714d77185fb73cf3f6
EduardoZeti <eduardo@zeti.software>
2021-12-20 12:35:58 -0600

    remove cffi from tool.maturin

---
+++ zarena/zarena/ffi.py
@@ -0,0 +1,8 @@
+# auto-generated file
+import _cffi_backend
+
+ffi = _cffi_backend.FFI('ffi',
+    _version = 0x2601,
+    _types = b'',
+    _globals = (b'\xFF\xFF\xFF\x1FBISHOP_ID',4,b'\xFF\xFF\xFF\x1FEMPTY_SQUARE_ID',0,b'\xFF\xFF\xFF\x1FKING_ID',1,b'\xFF\xFF\xFF\x1FKNIGHT_ID',5,b'\xFF\xFF\xFF\x1FPAWN_ID',6,b'\xFF\xFF\xFF\x1FQUEEN_ID',2,b'\xFF\xFF\xFF\x1FROOK_ID',3),
+)
