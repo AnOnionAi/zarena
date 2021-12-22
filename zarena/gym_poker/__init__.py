2171dc04d330c021059d82f6a21a601ad8c827dd
EduardoZeti <eduardo@zeti.software>
2021-12-22 16:31:44 -0600

    updated gym environment id's

--- zarena/gym_poker/__init__.py
+++ zarena/gym_poker/__init__.py
@@ -3,6 +3,6 @@ from zarena.gym_poker.envs.poker_env import PokerEnv  # envs
 from gym.envs.registration import register  # to register envs
 
 register(
-    id="PokerR-v1",
+    id="PokerEnv-v1",
     entry_point="zarena.gym_poker:PokerEnv",
 )
